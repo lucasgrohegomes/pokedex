78a265ba43a5ad7ed250546b95111e1c97a971e8
Lucas Grohe Gomes <lucas_g_gomes@estudante.sesisenai.org.br>
2024-05-23 12:25:13 -0300

    fim?

--- Atividade Pokedex/src/components/Pokedex.jsx
+++ Atividade Pokedex/src/components/Pokedex.jsx
@@ -1,8 +1,36 @@
-import React, { useState , useEffect} from 'react';
+import React, { useState , useEffect } from 'react';
 export default function Pokedex(){
-    return(
+    const{id, setId} = useState(1)
+    const{pokemon, setPokemon} = useState(null)
+
+    const fetchData = async () => {
+        try{
+            const response = await fetch(`https://pokepi.co/api/v2/pokemon/${id}`);
+            const data = await response.json();
+            setPokemon(data);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    useEffect(() =>{
+        fetchData()
+    }, [id])
+
+    const nextPokemon = () =>{
+        setId(id + 1)
+    }
+    return (
         <div>
-            <h1>Pokémon</h1>
+            {pokemon && (
+                <div className="pokemon">
+                    <h1>Pokémon</h1>
+                    <p>{pokemon.name}</p>
+                    <p>{pokemon.weight}</p>
+                    <img src={pokemon.sprites.front_default}></img>
+                    <button onClick={nextPokemon}>Próximo</button>
+                </div>
+            )} 
         </div>
     )
 }
